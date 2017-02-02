4d0d57593f55068c78aef9ddc1a99332c87c536f
Chanh Nguyen <chanhnguyen@Chanh-Nguyens-iMac.local>
2017-02-03 01:10:13 +0200

    Add Tweet framework, search text flied

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Chanh Nguyen on 2/2/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    
+    var tweet: Twitter.Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    fileprivate func updateUI()
+    {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        tweetCreatedLabel?.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = try? Data(contentsOf: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+            let formatter = DateFormatter()
+            if Date().timeIntervalSince(tweet.created) > 24*60*60 {
+                formatter.dateStyle = DateFormatter.Style.short
+            } else {
+                formatter.timeStyle = DateFormatter.Style.short
+            }
+            tweetCreatedLabel?.text = formatter.string(from: tweet.created)
+        }
+        
+    }
+    
+}
+
